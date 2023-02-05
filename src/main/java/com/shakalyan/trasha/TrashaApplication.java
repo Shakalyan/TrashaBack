f181c3d4794dcb7d86b85ef51d6f3d38557c4f12
Shakalyan <vdovit@mail.ru>
2023-02-05 19:01:59 +0300

    Initial commit

---
+++ src/main/java/com/shakalyan/trasha/TrashaApplication.java
@@ -0,0 +1,13 @@
+package com.shakalyan.trasha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shakalyan/trasha/TrashaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakalyan.trasha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
